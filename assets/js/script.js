7ba839a9b135a2daa2fcae73b445c27cec6e8c60
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-03-30 21:04:22 +1100

    fixed a bug that stopped the submit button from opening the results page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 var submitBtn = document.querySelector("#sumbit-button");
 
 submitBtn.addEventListener("click", function () {
-    window.location.href = "./results.html"
+    var cityName = document.querySelector("input").value;
+
+    console.log(cityName);
+
+    localStorage.setItem("cityName", cityName);
+
+    location.replace("./results.html")
 });
+
